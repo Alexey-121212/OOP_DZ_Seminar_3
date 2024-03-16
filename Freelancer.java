a16103f29bdfc6b25172669931f1578d40efb522
Alexey <alexmail121212@gmail.com>
2024-03-16 18:43:36 +0300

    freelancer done v1

--- Freelancer.java
+++ Freelancer.java
@@ -2,5 +2,25 @@
  * Фрилансер (работник с почасовой оплатой)
  * TODO: Доработать в рамках домашней работы
  */
-public class Freelancer {
+
+public class Freelancer extends Employee {
+
+    private Freelancer(String surName, String name, double salary) {
+        super(surName, name, salary);
+    }
+
+    public static Freelancer create(String surName, String name, double salary) {
+        return new Freelancer(surName, name, salary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return (20.8 * 8 * salary);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; ставка: %.2f руб.; заработная плата: %.2f руб.",
+                surName, name, salary, calculateSalary());
+    }
 }
